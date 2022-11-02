7481348278a023049bd6dd3f0a928a44e0ad231f
Dshoji <b221.d.shoji@gmail.com>
2022-11-02 12:16:22 +0900

    kadai02

---
+++ prmn2022a/src/lecture01/kadai02.java
@@ -0,0 +1,11 @@
+package lecture01;
+
+import java.util.Scanner;
+
+public class kadai02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int studentNumber = input.nextInt();
+        System.out.println("B" + studentNumber + " Daiki Shoji");
+    }
+}
