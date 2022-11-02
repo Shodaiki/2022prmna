e2ecbb97d5cde69584aba1d0a652aa72e4e39c41
Dshoji <b221.d.shoji@gmail.com>
2022-11-02 12:27:49 +0900

    kadai04

---
+++ prmn2022a/src/lecture01/kadai04.java
@@ -0,0 +1,15 @@
+package lecture01;
+
+public class kadai04 {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+            if (i % 2 == 0) {
+                sum = sum + array[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
